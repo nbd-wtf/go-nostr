ba0507cce78b43adffa758a6d7f3763a75691c3d
fiatjaf <fiatjaf@gmail.com>
2022-01-06 21:56:57 -0300

    nip06 package for key derivation.

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/btcsuite/btcd v0.20.1-beta // indirect
 	github.com/fiatjaf/bip340 v1.1.1
 	github.com/gorilla/websocket v1.4.2
+	github.com/tyler-smith/go-bip32 v1.0.0
+	github.com/tyler-smith/go-bip39 v1.1.0
 )
---
+++ nip06/nip06.go
@@ -0,0 +1,56 @@
+package nip06
+
+import (
+	"encoding/hex"
+
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
+)
+
+func GenerateSeedWords() (string, error) {
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		return "", err
+	}
+
+	words, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return "", err
+	}
+
+	return words, nil
+}
+
+func SeedFromWords(words string) []byte {
+	return bip39.NewSeed(words, "")
+}
+
+func PrivateKeyFromSeed(seed []byte) (string, error) {
+	key, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		return "", err
+	}
+
+	derivationPath := []uint32{
+		bip32.FirstHardenedChild + 44,
+		bip32.FirstHardenedChild + 1237,
+		bip32.FirstHardenedChild + 0,
+		0,
+		0,
+	}
+
+	next := key
+	for _, idx := range derivationPath {
+		var err error
+		next, err = next.NewChildKey(idx)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return hex.EncodeToString(next.Key), nil
+}
+
+func ValidateWords(words string) bool {
+	return bip39.IsMnemonicValid(words)
+}
