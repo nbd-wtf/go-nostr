66aa4e9de2e3ab7a62c94cc653d0a884440e982c
fiatjaf <fiatjaf@gmail.com>
2022-07-11 15:51:24 -0300

    add nip-11 type.

---
+++ nip11/types.go
@@ -0,0 +1,11 @@
+package nip11
+
+type RelayInformationDocument struct {
+	Name          string `json:"name"`
+	Description   string `json:"description"`
+	PubKey        string `json:"pubkey"`
+	Contact       string `json:"contact"`
+	SupportedNIPs []int  `json:"supported_nips"`
+	Software      string `json:"software"`
+	Version       string `json:"version"`
+}
