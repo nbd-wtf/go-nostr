4dbbcec80a7f389eddbe9ef989b3321c131ccfd7
fiatjaf <fiatjaf@gmail.com>
2022-12-29 09:35:05 -0300

    one more nip19 test.

--- nip19/nip19_test.go
+++ nip19/nip19_test.go
@@ -1,7 +1,6 @@
 package nip19
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -58,8 +57,6 @@ func TestDecodeNprofile(t *testing.T) {
 		t.Error("value returned of wrong type")
 	}
 
-	fmt.Println(pp)
-
 	if pp.PublicKey != "3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d" {
 		t.Error("decoded invalid public key")
 	}
@@ -68,6 +65,31 @@ func TestDecodeNprofile(t *testing.T) {
 		t.Error("decoded wrong number of relays")
 	}
 	if pp.Relays[0] != "wss://r.x.com" || pp.Relays[1] != "wss://djbas.sadkb.com" {
+		t.Error("decoded relay URLs wrongly")
+	}
+}
+
+func TestDecodeOtherNprofile(t *testing.T) {
+	prefix, data, err := Decode("nprofile1qqsw3dy8cpumpanud9dwd3xz254y0uu2m739x0x9jf4a9sgzjshaedcpr4mhxue69uhkummnw3ez6ur4vgh8wetvd3hhyer9wghxuet5qyw8wumn8ghj7mn0wd68yttjv4kxz7fww4h8get5dpezumt9qyvhwumn8ghj7un9d3shjetj9enxjct5dfskvtnrdakstl69hg")
+	if err != nil {
+		t.Error("failed to decode nprofile")
+	}
+	if prefix != "nprofile" {
+		t.Error("what")
+	}
+	pp, ok := data.(ProfilePointer)
+	if !ok {
+		t.Error("value returned of wrong type")
+	}
+
+	if pp.PublicKey != "e8b487c079b0f67c695ae6c4c2552a47f38adfa2533cc5926bd2c102942fdcb7" {
+		t.Error("decoded invalid public key")
+	}
+
+	if len(pp.Relays) != 3 {
+		t.Error("decoded wrong number of relays")
+	}
+	if pp.Relays[0] != "wss://nostr-pub.wellorder.net" || pp.Relays[1] != "wss://nostr-relay.untethr.me" {
 		t.Error("decoded relay URLs wrongly")
 	}
 }
