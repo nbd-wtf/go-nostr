cc06f3b9098082e35ba24221568a048bff784038
Patrick Bennett <patrick+git@bennettfamily.org>
2023-04-25 23:52:56 -0400

    Add some NIP-11 extension structures to the RelayInformationDocument struct.
    Added additional NIP-11 fields for relays that want to provide additional details based on NIP-11 extensions.
    The retention structure has been left out as it doesn't have a clean schema for kinds (array of kinds, or pairs of kinds?)
    Specified the fields w/ omitempty so marshaled will be same as original NIP-11 if nothing else is specified.
    Nested structs defined as pointers so they are omitted if not specified.

--- nip11/types.go
+++ nip11/types.go
@@ -8,4 +8,43 @@ type RelayInformationDocument struct {
 	SupportedNIPs []int  `json:"supported_nips"`
 	Software      string `json:"software"`
 	Version       string `json:"version"`
+
+	Limitation     *RelayLimitationDocument `json:"limitation,omitempty"`
+	RelayCountries []string                 `json:"relay_countries,omitempty"`
+	LanguageTags   []string                 `json:"language_tags,omitempty"`
+	Tags           []string                 `json:"tags,omitempty"`
+	PostingPolicy  string                   `json:"posting_policy,omitempty"`
+	PaymentsURL    string                   `json:"payments_url,omitempty"`
+	Fees           *RelayFeesDocument       `json:"fees,omitempty"`
+}
+
+type RelayLimitationDocument struct {
+	MaxMessageLength int  `json:"max_message_length,omitempty"`
+	MaxSubscriptions int  `json:"max_subscriptions,omitempty"`
+	MaxFilters       int  `json:"max_filters,omitempty"`
+	MaxLimit         int  `json:"max_limit,omitempty"`
+	MaxSubidLength   int  `json:"max_subid_length,omitempty"`
+	MinPrefix        int  `json:"min_prefix,omitempty"`
+	MaxEventTags     int  `json:"max_event_tags,omitempty"`
+	MaxContentLength int  `json:"max_content_length,omitempty"`
+	MinPowDifficulty int  `json:"min_pow_difficulty,omitempty"`
+	AuthRequired     bool `json:"auth_required"`
+	PaymentRequired  bool `json:"payment_required"`
+}
+
+type RelayFeesDocument struct {
+	Admission []struct {
+		Amount int    `json:"amount"`
+		Unit   string `json:"unit"`
+	} `json:"admission,omitempty"`
+	Subscription []struct {
+		Amount int    `json:"amount"`
+		Unit   string `json:"unit"`
+		Period int    `json:"period"`
+	} `json:"subscription,omitempty"`
+	Publication []struct {
+		Kinds  []int  `json:"kinds"`
+		Amount int    `json:"amount"`
+		Unit   string `json:"unit"`
+	} `json:"publication,omitempty"`
 }
