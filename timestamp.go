902b23c3126bbf43ec1cbf960ad5adbf9ef5cfce
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-05-06 03:57:31 +0900

    add Before/After for Timestamp

--- timestamp.go
+++ timestamp.go
@@ -11,3 +11,21 @@ func Now() Timestamp {
 func (t Timestamp) Time() time.Time {
 	return time.Unix(int64(t), 0)
 }
+
+func (t Timestamp) Before(u Timesamp) bool {
+	return t < u
+}
+
+func (t Timestamp) After(u Timesamp) bool {
+	return t > u
+}
+
+func (t Timestamp) Compare(u Time) int {
+	switch {
+	case t < u:
+		return -1
+	case t > u:
+		return +1
+	}
+	return 0
+}
