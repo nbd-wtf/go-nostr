1cd2c0293d8aaa26d535b6fae4653c152ca06e0e
fiatjaf <fiatjaf@gmail.com>
2023-05-28 15:44:17 -0300

    add a bunch more of event kinds.

--- event.go
+++ event.go
@@ -24,21 +24,38 @@ type Event struct {
 }
 
 const (
-	KindSetMetadata            int = 0
-	KindTextNote               int = 1
-	KindRecommendServer        int = 2
-	KindContactList            int = 3
-	KindEncryptedDirectMessage int = 4
-	KindDeletion               int = 5
-	KindBoost                  int = 6
-	KindReaction               int = 7
-	KindChannelCreation        int = 40
-	KindChannelMetadata        int = 41
-	KindChannelMessage         int = 42
-	KindChannelHideMessage     int = 43
-	KindChannelMuteUser        int = 44
-	KindZapRequest             int = 9734
-	KindZap                    int = 9735
+	KindSetMetadata              int = 0
+	KindTextNote                 int = 1
+	KindRecommendServer          int = 2
+	KindContactList              int = 3
+	KindEncryptedDirectMessage   int = 4
+	KindDeletion                 int = 5
+	KindBoost                    int = 6
+	KindReaction                 int = 7
+	KindChannelCreation          int = 40
+	KindChannelMetadata          int = 41
+	KindChannelMessage           int = 42
+	KindChannelHideMessage       int = 43
+	KindChannelMuteUser          int = 44
+	KindFileMetadata             int = 1063
+	KindZapRequest               int = 9734
+	KindZap                      int = 9735
+	KindMuteList                 int = 10000
+	KindPinList                  int = 10001
+	KindRelayListMetadata        int = 10002
+	KindNWCWalletInfo            int = 13194
+	KindClientAuthentication     int = 22242
+	KindNWCWalletRequest         int = 23194
+	KindNWCWalletResponse        int = 23195
+	KindNostrConnect             int = 24133
+	KindCategorizedPeopleList    int = 30000
+	KindCategorizedBookmarksList int = 30001
+	KindProfileBadges            int = 30008
+	KindBadgeDefinition          int = 30009
+	KindStallDefinition          int = 30017
+	KindProductDefinition        int = 30018
+	KindArticle                  int = 30023
+	KindApplicationSpecificData  int = 30078
 )
 
 // Event Stringer interface, just returns the raw JSON as a string
