a9972245f37043d43d95d39beb6023d0ddfe92d1
fiatjaf <fiatjaf@gmail.com>
2023-12-02 13:13:32 -0300

    nip04: augment tests with cross-compatibility vectors.

--- nip04/nip04_test.go
+++ nip04/nip04_test.go
@@ -3,6 +3,8 @@ package nip04
 import (
 	"strings"
 	"testing"
+
+	"github.com/nbd-wtf/go-nostr"
 )
 
 func TestEncryptionAndDecryption(t *testing.T) {
@@ -43,5 +45,17 @@ func TestEncryptionAndDecryptionWithMultipleLengths(t *testing.T) {
 		if message != plaintext {
 			t.Errorf("original '%s' and decrypted '%s' messages differ", message, plaintext)
 		}
+	}
+}
+
+func TestNostrToolsCompatibility(t *testing.T) {
+	sk1 := "92996316beebf94171065a714cbf164d1f56d7ad9b35b329d9fc97535bf25352"
+	sk2 := "591c0c249adfb9346f8d37dfeed65725e2eea1d7a6e99fa503342f367138de84"
+	pk2, _ := nostr.GetPublicKey(sk2)
+	shared, _ := ComputeSharedSecret(pk2, sk1)
+	ciphertext := "A+fRnU4aXS4kbTLfowqAww==?iv=QFYUrl5or/n/qamY79ze0A=="
+	plaintext, _ := Decrypt(ciphertext, shared)
+	if plaintext != "hello" {
+		t.Fatal("invalid decryption of nostr-tools payload")
 	}
 }
