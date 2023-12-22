f1980e8b7a0cdf496083e05476550937786d834d
fiatjaf <fiatjaf@gmail.com>
2023-12-22 18:18:44 -0300

    expose max limits allowed for binary format event fields.

---
+++ binary/limits.go
@@ -0,0 +1,10 @@
+package binary
+
+import "math"
+
+const (
+	MaxContentSize = math.MaxUint16
+	MaxKind        = math.MaxUint16
+	MaxTagCount    = math.MaxUint16
+	MaxTagSize     = math.MaxUint16
+)
