6f32e4da63bb7a14b1903a5c3d5d25abead334f2
fiatjaf <fiatjaf@gmail.com>
2024-03-13 16:36:08 -0300

    nip54: improve normalization and add tests.

--- nip54/nip54.go
+++ nip54/nip54.go
@@ -1,17 +1,26 @@
 package nip54
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 
 	"golang.org/x/text/transform"
 	"golang.org/x/text/unicode/norm"
 )
 
-var nonLetter = regexp.MustCompile(`\W`)
-
 func NormalizeIdentifier(name string) string {
+	name = strings.ToLower(name)
 	res, _, _ := transform.Bytes(norm.NFKC, []byte(name))
-	str := nonLetter.ReplaceAllString(string(res), "-")
-	return strings.ToLower(str)
+	runes := []rune(string(res))
+
+	b := make([]rune, len(runes))
+	for i, letter := range runes {
+		if unicode.IsLetter(letter) {
+			b[i] = letter
+		} else {
+			b[i] = '-'
+		}
+	}
+
+	return string(b)
 }
---
+++ nip54/nip54_test.go
@@ -0,0 +1,23 @@
+package nip54
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNormalization(t *testing.T) {
+	for _, vector := range []struct {
+		before string
+		after  string
+	}{
+		{"hello", "hello"},
+		{"Goodbye", "goodbye"},
+		{"the long and winding road / that leads to your door", "the-long-and-winding-road---that-leads-to-your-door"},
+		{"it's 平仮名", "it-s-平仮名"},
+	} {
+		if norm := NormalizeIdentifier(vector.before); norm != vector.after {
+			fmt.Println([]byte(vector.after), []byte(norm))
+			t.Fatalf("%s: %s != %s", vector.before, norm, vector.after)
+		}
+	}
+}
