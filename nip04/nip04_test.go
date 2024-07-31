1bed62c7afac15ad799c82f7ef16cf32d7f8a71c
fiatjaf <fiatjaf@gmail.com>
2024-07-31 20:49:06 -0300

    nip04: one more test to make @mleku happy.

--- nip04/nip04_test.go
+++ nip04/nip04_test.go
@@ -5,25 +5,39 @@ import (
 	"testing"
 
 	"github.com/nbd-wtf/go-nostr"
+	"github.com/stretchr/testify/require"
 )
+
+func TestSharedKeysAreTheSame(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		sk1 := nostr.GeneratePrivateKey()
+		sk2 := nostr.GeneratePrivateKey()
+
+		pk1, err := nostr.GetPublicKey(sk1)
+		require.NoError(t, err)
+		pk2, err := nostr.GetPublicKey(sk2)
+		require.NoError(t, err)
+
+		ss1, err := ComputeSharedSecret(pk2, sk1)
+		require.NoError(t, err)
+		ss2, err := ComputeSharedSecret(pk1, sk2)
+		require.NoError(t, err)
+
+		require.Equal(t, ss1, ss2)
+	}
+}
 
 func TestEncryptionAndDecryption(t *testing.T) {
 	sharedSecret := make([]byte, 32)
 	message := "hello hello"
 
 	ciphertext, err := Encrypt(message, sharedSecret)
-	if err != nil {
-		t.Errorf("failed to encrypt: %s", err.Error())
-	}
+	require.NoError(t, err)
 
 	plaintext, err := Decrypt(ciphertext, sharedSecret)
-	if err != nil {
-		t.Errorf("failed to decrypt: %s", err.Error())
-	}
+	require.NoError(t, err)
 
-	if message != plaintext {
-		t.Errorf("original '%s' and decrypted '%s' messages differ", message, plaintext)
-	}
+	require.Equal(t, plaintext, message, "original '%s' and decrypted '%s' messages differ", message, plaintext)
 }
 
 func TestEncryptionAndDecryptionWithMultipleLengths(t *testing.T) {
@@ -33,18 +47,12 @@ func TestEncryptionAndDecryptionWithMultipleLengths(t *testing.T) {
 		message := strings.Repeat("a", i)
 
 		ciphertext, err := Encrypt(message, sharedSecret)
-		if err != nil {
-			t.Errorf("failed to encrypt: %s", err.Error())
-		}
+		require.NoError(t, err)
 
 		plaintext, err := Decrypt(ciphertext, sharedSecret)
-		if err != nil {
-			t.Errorf("failed to decrypt: %s", err.Error())
-		}
+		require.NoError(t, err)
 
-		if message != plaintext {
-			t.Errorf("original '%s' and decrypted '%s' messages differ", message, plaintext)
-		}
+		require.Equal(t, plaintext, message, "original '%s' and decrypted '%s' messages differ", message, plaintext)
 	}
 }
 
@@ -55,7 +63,5 @@ func TestNostrToolsCompatibility(t *testing.T) {
 	shared, _ := ComputeSharedSecret(pk2, sk1)
 	ciphertext := "A+fRnU4aXS4kbTLfowqAww==?iv=QFYUrl5or/n/qamY79ze0A=="
 	plaintext, _ := Decrypt(ciphertext, shared)
-	if plaintext != "hello" {
-		t.Fatal("invalid decryption of nostr-tools payload")
-	}
+	require.Equal(t, "hello", plaintext, "invalid decryption of nostr-tools payload")
 }
