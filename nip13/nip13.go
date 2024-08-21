75e6befdd0f83d4d5be774fa50ed2cb116bdc993
fiatjaf <fiatjaf@gmail.com>
2024-08-21 16:54:02 -0300

    nip13: move Generate() to deprecated.go

--- nip13/nip13.go
+++ nip13/nip13.go
@@ -7,7 +7,6 @@ import (
 	"math/bits"
 	"runtime"
 	"strconv"
-	"time"
 
 	nostr "github.com/nbd-wtf/go-nostr"
 )
@@ -63,30 +62,6 @@ func Check(id string, minDifficulty int) error {
 		return ErrDifficultyTooLow
 	}
 	return nil
-}
-
-// Deprecated: use DoWork()
-func Generate(event *nostr.Event, targetDifficulty int, timeout time.Duration) (*nostr.Event, error) {
-	if event.PubKey == "" {
-		return nil, ErrMissingPubKey
-	}
-
-	tag := nostr.Tag{"nonce", "", strconv.Itoa(targetDifficulty)}
-	event.Tags = append(event.Tags, tag)
-	var nonce uint64
-	start := time.Now()
-	for {
-		nonce++
-		tag[1] = strconv.FormatUint(nonce, 10)
-		if Difficulty(event.GetID()) >= targetDifficulty {
-			return event, nil
-		}
-		// benchmarks show one iteration is approx 3000ns on i7-8565U @ 1.8GHz.
-		// so, check every 30ms; arbitrary
-		if nonce%10000 == 0 && time.Since(start) > timeout {
-			return nil, ErrGenerateTimeout
-		}
-	}
 }
 
 // DoWork() performs work in multiple threads (given by runtime.NumCPU()) and returns the first
