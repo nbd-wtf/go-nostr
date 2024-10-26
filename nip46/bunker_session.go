579172e5b73198b0aab4d59ba1928013ce9b2472
fiatjaf <fiatjaf@gmail.com>
2024-10-26 19:40:25 -0300

    nip46: move some code around for clarity.

---
+++ nip46/bunker_session.go
@@ -0,0 +1,67 @@
+package nip46
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip04"
+	"github.com/nbd-wtf/go-nostr/nip44"
+)
+
+type Session struct {
+	PublicKey       string
+	SharedKey       []byte   // nip04
+	ConversationKey [32]byte // nip44
+}
+
+type RelayReadWrite struct {
+	Read  bool `json:"read"`
+	Write bool `json:"write"`
+}
+
+func (s Session) ParseRequest(event *nostr.Event) (Request, error) {
+	var req Request
+
+	plain, err := nip44.Decrypt(event.Content, s.ConversationKey)
+	if err != nil {
+		plain, err = nip04.Decrypt(event.Content, s.SharedKey)
+		if err != nil {
+			return req, fmt.Errorf("failed to decrypt event from %s: %w", event.PubKey, err)
+		}
+	}
+
+	err = json.Unmarshal([]byte(plain), &req)
+	return req, err
+}
+
+func (s Session) MakeResponse(
+	id string,
+	requester string,
+	result string,
+	err error,
+) (resp Response, evt nostr.Event, error error) {
+	if err != nil {
+		resp = Response{
+			ID:    id,
+			Error: err.Error(),
+		}
+	} else if result != "" {
+		resp = Response{
+			ID:     id,
+			Result: result,
+		}
+	}
+
+	jresp, _ := json.Marshal(resp)
+	ciphertext, err := nip04.Encrypt(string(jresp), s.SharedKey)
+	if err != nil {
+		return resp, evt, fmt.Errorf("failed to encrypt result: %w", err)
+	}
+	evt.Content = ciphertext
+	evt.CreatedAt = nostr.Now()
+	evt.Kind = nostr.KindNostrConnect
+	evt.Tags = nostr.Tags{nostr.Tag{"p", requester}}
+
+	return resp, evt, nil
+}
