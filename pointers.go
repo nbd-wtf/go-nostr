159e5d21e6c3729c19bda385db5f4a128f0989c2
fiatjaf <fiatjaf@gmail.com>
2025-01-01 15:04:54 -0300

    some mildly useful helpers for pointers in general.

--- pointers.go
+++ pointers.go
@@ -1,8 +1,26 @@
 package nostr
 
+import (
+	"fmt"
+)
+
+type Pointer interface {
+	AsTagReference() string
+	AsTag() Tag
+}
+
 type ProfilePointer struct {
 	PublicKey string   `json:"pubkey"`
 	Relays    []string `json:"relays,omitempty"`
+}
+
+func (ep ProfilePointer) AsTagReference() string { return ep.PublicKey }
+
+func (ep ProfilePointer) AsTag() Tag {
+	if len(ep.Relays) > 0 {
+		return Tag{"p", ep.PublicKey, ep.Relays[0]}
+	}
+	return Tag{"p", ep.PublicKey}
 }
 
 type EventPointer struct {
@@ -12,9 +30,33 @@ type EventPointer struct {
 	Kind   int      `json:"kind,omitempty"`
 }
 
+func (ep EventPointer) AsTagReference() string { return ep.ID }
+
+func (ep EventPointer) AsTag() Tag {
+	if len(ep.Relays) > 0 {
+		if ep.Author != "" {
+			return Tag{"e", ep.ID, ep.Relays[0], ep.Author}
+		} else {
+			return Tag{"e", ep.ID, ep.Relays[0]}
+		}
+	}
+	return Tag{"e", ep.ID}
+}
+
 type EntityPointer struct {
 	PublicKey  string   `json:"pubkey"`
 	Kind       int      `json:"kind,omitempty"`
 	Identifier string   `json:"identifier,omitempty"`
 	Relays     []string `json:"relays,omitempty"`
+}
+
+func (ep EntityPointer) AsTagReference() string {
+	return fmt.Sprintf("%d:%s:%s", ep.Kind, ep.PublicKey, ep.Identifier)
+}
+
+func (ep EntityPointer) AsTag() Tag {
+	if len(ep.Relays) > 0 {
+		return Tag{"a", ep.AsTagReference(), ep.Relays[0]}
+	}
+	return Tag{"a", ep.AsTagReference()}
 }
