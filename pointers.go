550cf835be3242c925e5beaec2815556c3a7478f
fiatjaf <fiatjaf@gmail.com>
2025-01-23 16:53:15 -0300

    pointer.MatchesEvent()

--- pointers.go
+++ pointers.go
@@ -7,6 +7,7 @@ import (
 type Pointer interface {
 	AsTagReference() string
 	AsTag() Tag
+	MatchesEvent(Event) bool
 }
 
 type ProfilePointer struct {
@@ -14,7 +15,8 @@ type ProfilePointer struct {
 	Relays    []string `json:"relays,omitempty"`
 }
 
-func (ep ProfilePointer) AsTagReference() string { return ep.PublicKey }
+func (ep ProfilePointer) MatchesEvent(_ Event) bool { return false }
+func (ep ProfilePointer) AsTagReference() string    { return ep.PublicKey }
 
 func (ep ProfilePointer) AsTag() Tag {
 	if len(ep.Relays) > 0 {
@@ -30,7 +32,8 @@ type EventPointer struct {
 	Kind   int      `json:"kind,omitempty"`
 }
 
-func (ep EventPointer) AsTagReference() string { return ep.ID }
+func (ep EventPointer) MatchesEvent(evt Event) bool { return evt.ID == ep.ID }
+func (ep EventPointer) AsTagReference() string      { return ep.ID }
 
 func (ep EventPointer) AsTag() Tag {
 	if len(ep.Relays) > 0 {
@@ -48,6 +51,12 @@ type EntityPointer struct {
 	Kind       int      `json:"kind,omitempty"`
 	Identifier string   `json:"identifier,omitempty"`
 	Relays     []string `json:"relays,omitempty"`
+}
+
+func (ep EntityPointer) MatchesEvent(evt Event) bool {
+	return ep.PublicKey == evt.PubKey &&
+		ep.Kind == evt.Kind &&
+		evt.Tags.GetD() == ep.Identifier
 }
 
 func (ep EntityPointer) AsTagReference() string {
