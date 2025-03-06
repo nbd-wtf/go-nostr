ec55b1fac8caf0bb10f90a2b0dfc94e949629aaf
fiatjaf <fiatjaf@gmail.com>
2025-03-06 11:49:10 -0300

    nip19: decode to pointer.

--- nip19/pointer.go
+++ nip19/pointer.go
@@ -1,6 +1,10 @@
 package nip19
 
-import "github.com/nbd-wtf/go-nostr"
+import (
+	"fmt"
+
+	"github.com/nbd-wtf/go-nostr"
+)
 
 func EncodePointer(pointer nostr.Pointer) string {
 	switch v := pointer.(type) {
@@ -15,4 +19,26 @@ func EncodePointer(pointer nostr.Pointer) string {
 		return res
 	}
 	return ""
+}
+
+func ToPointer(code string) (nostr.Pointer, error) {
+	prefix, data, err := Decode(code)
+	if err != nil {
+		return nil, err
+	}
+
+	switch prefix {
+	case "npub":
+		return nostr.ProfilePointer{PublicKey: data.(string)}, nil
+	case "nprofile":
+		return data.(nostr.ProfilePointer), nil
+	case "nevent":
+		return data.(nostr.EventPointer), nil
+	case "note":
+		return nostr.EventPointer{ID: data.(string)}, nil
+	case "naddr":
+		return data.(nostr.EntityPointer), nil
+	default:
+		return nil, fmt.Errorf("unexpected prefix '%s' to '%s'", prefix, code)
+	}
 }
--- nip19/utils.go
+++ nip19/utils.go
@@ -1,6 +1,8 @@
 package nip19
 
-import "github.com/nbd-wtf/go-nostr"
+import (
+	"github.com/nbd-wtf/go-nostr"
+)
 
 func NeventFromRelayEvent(ie nostr.RelayEvent) string {
 	v, _ := EncodeEvent(ie.ID, []string{ie.Relay.URL}, ie.PubKey)
