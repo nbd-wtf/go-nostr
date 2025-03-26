6fc68dc03983f2808b39f8b6a65af8149e53af34
fiatjaf <fiatjaf@gmail.com>
2025-03-26 12:56:55 -0300

    nip70: HasEmbeddedProtected()

--- nip70/nip70.go
+++ nip70/nip70.go
@@ -1,12 +1,29 @@
 package nip70
 
-import "github.com/nbd-wtf/go-nostr"
+import (
+	"strings"
 
-func IsProtected(event *nostr.Event) bool {
+	"github.com/nbd-wtf/go-nostr"
+)
+
+func IsProtected(event nostr.Event) bool {
 	for _, tag := range event.Tags {
 		if len(tag) == 1 && tag[0] == "-" {
 			return true
 		}
 	}
+	return false
+}
+
+func HasEmbeddedProtected(event nostr.Event) bool {
+	if event.Kind == 6 || event.Kind == 16 {
+		tidx := strings.Index(event.Content, `"tags":[`)
+		eidx := strings.Index(event.Content, `]]`)
+		pidx := strings.Index(event.Content, `["-"]`)
+		if tidx != -1 && eidx != -1 && pidx != -1 {
+			return pidx > tidx && pidx < eidx
+		}
+	}
+
 	return false
 }
